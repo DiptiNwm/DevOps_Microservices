05a75deab8e44e3691e3f15d4da9d9e2ff3473ee
EC2 Default User <ec2-user@ip-172-31-8-2.us-east-2.compute.internal>
2021-08-23 13:49:18 +0000

    adding new files

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=diptinwm/devopsproject4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+minikube kubectl run latest --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+minikube kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+minikube kubectl port-forward latest 8000:80
